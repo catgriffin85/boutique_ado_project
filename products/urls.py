71e70b9de579f4e503bd04411448ff498eb5771e
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2025-01-26 20:56:27 +0000

    Start product template

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from .import views
 
 
-from django.urls import path
-from .views import all_products
-
 urlpatterns = [
-    path('', all_products, name='all_products'),
+    path('', views.all_products, name='all_products')
 ]
