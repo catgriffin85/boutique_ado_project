a4595b5a6c3493ee939a6134b35421ba93b34bdf
catgriffin85 <catherine.griffin@students.codeinstitute.net>
2025-02-26 22:22:52 +0000

    last heroku attempt for the day

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -33,9 +33,9 @@ DEBUG = 'DEVELOPMENT' in os.environ
 #ALLOWED_HOSTS = ['*',
 #]
 
-ALLOWED_HOSTS = ['127.0.0.1',  
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost',
                  'boutique-ado-walkthrough-cg-ca8cc69d19fd.herokuapp.com/',
-                 'https://boutique-ado-walkthrough-cg-ca8cc69d19fd.herokuapp.com/',
 ]
 
 
